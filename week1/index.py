6c1630d5782ca45d964a7825a3f105588b2212e1
WatsonWaswa <watsonliwa@yahoo.com>
2025-03-11 11:47:32 +0300

    update on Intro to python

--- week1/index.py
+++ week1/index.py
@@ -1,1 +1,25 @@
 print ("Hello world")
+
+number = 10
+print (number)
+
+
+
+
+
+age = 27         #int datatype
+marks = 90.5     #float datatype
+
+name = "My name is Watson Waswa" #string datatype
+
+
+
+
+
+#LISTS DATA TYPES
+
+languages = ["Python", "HTML", "CSS", 3, 5.7, True]  #can gold different datatypes
+print(languages)
+print(languages[2])
+print(languages[5])
+print(languages[-5])
